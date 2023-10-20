f0e93fa76cee6bb1dddfd0c2cead4ba094667790
Tomás De Donatis <tomasdedonatis@hotmail.com>
2023-10-20 03:42:05 -0300

    Update MovementController.cs

--- Assets/Entrega/Scripts/Entity/Player/MovementController.cs
+++ Assets/Entrega/Scripts/Entity/Player/MovementController.cs
@@ -5,51 +5,58 @@ using UnityEngine.EventSystems;
 
 public class MovementController : MonoBehaviour
 {
-    public float speed;
+    /*[Header("Cambio MovInput Keybord/Mobile")]
+    public bool _keyboardInputs;
+    public float speed;*/
+
+    private float _controlX, _controlY;
     Vector2 _dragDirection;
     Vector2 _screenBounds;
-
-    [Header("Cambio MovInput Keybord/Mobile")]
-    public bool _keyboardInputs;
 
     private void Update()
     {
         _screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
-        // _playerWidth = transform.GetComponent<BoxCollider>().size.x * 0.5f;
-        // _playerHeight = transform.GetComponent<BoxCollider>().size.y * 0.5f;
-        if (_keyboardInputs)
+        MobileInputs();
+        /*if (_keyboardInputs)
         {
             KeyboardInput();
         }
         else
         {
             MobileInputs();
-        }
-
+        }*/
     }
 
-    void KeyboardInput()
-    {
-        Vector3 dir = new Vector3();
-        dir.x = Input.GetAxisRaw("Horizontal");
-        dir.y = Input.GetAxisRaw("Vertical");
-        transform.position += dir * speed * Time.deltaTime;
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, (_screenBounds.x * -1), _screenBounds.x), Mathf.Clamp(transform.position.y, (_screenBounds.y * -1), _screenBounds.y), 0);
-    }
 
     void MobileInputs()
     {
         if (Input.touchCount < 1) return;
 
         Touch firstTouch = Input.touches[0];
+        Vector2 touchPos = Camera.main.ScreenToWorldPoint(firstTouch.position);
 
-        if (firstTouch.phase == TouchPhase.Moved)
+        switch (firstTouch.phase)
         {
-            _dragDirection = firstTouch.deltaPosition;
-
-            transform.position += ((Vector3)_dragDirection * Time.deltaTime);
-            transform.position = new Vector3(Mathf.Clamp(transform.position.x, (_screenBounds.x * -1), _screenBounds.x), Mathf.Clamp(transform.position.y, (_screenBounds.y * -1), _screenBounds.y), 0);
+            case TouchPhase.Began:
+                _controlX = touchPos.x - transform.position.x;
+                _controlY = touchPos.y - transform.position.y;
+                break;
+            case TouchPhase.Moved:
+                transform.position = new Vector2(touchPos.x - _controlX,transform.position.y /*touchPos.y - _controlY*/);
+                transform.position = new Vector3(Mathf.Clamp(transform.position.x, (_screenBounds.x * -1), _screenBounds.x), Mathf.Clamp(transform.position.y, (_screenBounds.y * -1), _screenBounds.y), 0);
+                break;
+            case TouchPhase.Ended:
+                transform.position = transform.position;
+                break;
         }
     }
 
+    /*void KeyboardInput()
+    {
+        Vector3 dir = new Vector3();
+        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.y = Input.GetAxisRaw("Vertical");
+        transform.position += dir * speed * Time.deltaTime;
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, (_screenBounds.x * -1), _screenBounds.x), Mathf.Clamp(transform.position.y, (_screenBounds.y * -1), _screenBounds.y), 0);
+    }*/
 }
