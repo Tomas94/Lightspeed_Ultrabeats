d1b412cb914e90f204133ba052ebec990a59e814
NickJArt <iliveforplay@gmail.com>
2023-12-08 13:16:42 -0300

    Arreglado brillo y añadida funcionalidad de toggle de volumen

--- Assets/Entrega/Scripts/System/UI/OptionsMenu.cs
+++ Assets/Entrega/Scripts/System/UI/OptionsMenu.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
@@ -6,11 +7,15 @@ public class OptionsMenu : MonoBehaviour
     [SerializeField] Toggle _vibrationAllowed;
     [SerializeField] Image _brightnessBG;
     [SerializeField] Slider _brightnessSlider;
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] Toggle soundToggle;
+
 
     private void Start()
     {
-       // _brightnessSlider.value = GameManager.Instance.brigthnessValue;
+        // _brightnessSlider.value = GameManager.Instance.brigthnessValue;
         //_vibrationAllowed.isOn = GameManager.Instance.vibration;
+        soundToggle.isOn = IsSoundOn();
     }
 
     public void VibrationSwapState()
@@ -27,4 +32,31 @@ public class OptionsMenu : MonoBehaviour
         //GameManager.Instance.brigthnessValue = _brightnessSlider.value;
         PlayerPrefs.SetFloat("BrightnessValue", _brightnessSlider.value);
     }
+
+    #region Volumen
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void ToggleSound()
+    {
+        bool soundOn = soundToggle.isOn;
+        SetSoundState(soundOn);
+    }
+
+    private bool IsSoundOn()
+    {
+        float volume;
+        audioMixer.GetFloat("Volume", out volume);
+        return volume > -80; // o cualquier otro valor que consideres como "activado"
+    }
+
+    private void SetSoundState(bool soundOn)
+    {
+        float volume = soundOn ? 0.0f : -80.0f; // 0 para activado, -80 (u otro) para desactivado
+        SetVolume(volume);
+    }
+    #endregion
+
 }
